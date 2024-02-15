53f977e9213d4a5086111060cec9ec589049942a
Delstun McCray <87548497+Delstroo@users.noreply.github.com>
2024-02-15 13:02:15 -0800

    pushing updates

--- Week1-Conditionals/house.py
+++ Week1-Conditionals/house.py
@@ -1,5 +1,6 @@
 name = input("What's your name? ")
 
+# This could be done with 'if' and 'elif', however this is a clean way to integrate this way to check on what the name the user is and determine what house they will be in.
 match name: 
     case "Harry" | "Hermione" | "Ron":
         print("Gryffindor")
